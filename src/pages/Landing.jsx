87cd69556e470ba802cb8c42c0e4e7027b9c5f52
MANAS BHANDARI <107706027+manasbhandari@users.noreply.github.com>
2024-02-10 20:55:07 +0530

    Add files via upload

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+const Landing = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  )
+}
+
+export default Landing
